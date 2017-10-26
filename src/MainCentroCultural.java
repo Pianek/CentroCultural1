1982681b36bc303dbc0e7de395f74ce8c4e72d30
Ana <Ana@192.168.1.39>
2017-10-26 20:30:49 +0200

    Main con todos los ejemplos

--- src/MainCentroCultural.java
+++ src/MainCentroCultural.java
@@ -2,19 +2,9 @@
 public class MainCentroCultural {
 
 	public static void main(String[] args) {
-
-//		Conexion conn= new Conexion();
 //		FrontAlquilarCD alquilarCD = new FrontAlquilarCD();
-
-//		FrontAlquilarCD alquilarCD = new FrontAlquilarCD();
-<<<<<<< HEAD
 		FrontAlquilarDVD alquilarDVD = new FrontAlquilarDVD();
 //		FrontAlquilarLibro alquilarLibro = new FrontAlquilarLibro();
-=======
-//		FrontAlquilarDVD alquilarDVD = new FrontAlquilarDVD();
-		FrontAlquilarLibro alquilarLibro = new FrontAlquilarLibro();
-
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
 //		FrontLogin login = new FrontLogin(); 
 //		FrontArticulo articulo = new FrontArticulo();
 //		FrontAdmin admin = new FrontAdmin();
