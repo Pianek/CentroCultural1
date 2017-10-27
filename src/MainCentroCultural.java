069c3d610c6826f349c97878d130a6d3aa50bdc2
Ana <Ana@192.168.1.39>
2017-10-27 19:13:30 +0200

    *

--- src/MainCentroCultural.java
+++ src/MainCentroCultural.java
@@ -6,23 +6,11 @@ public class MainCentroCultural {
 //		FrontAlquilarDVD alquilarDVD = new FrontAlquilarDVD();
 //		FrontAlquilarLibro alquilarLibro = new FrontAlquilarLibro();
 //		FrontLogin login = new FrontLogin(); 
-<<<<<<< HEAD
-=======
-		//FrontArticulo articulo = new FrontArticulo();
-		//FrontAlquilar  alquilar= new FrontAlquilar();
-		//FrontActualizarBorrar AB = new FrontActualizarBorrar();
-		FrontDevolver dv= new FrontDevolver();
-
-		//FrontLogin login = new FrontLogin(); 
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
-//		FrontArticulo articulo = new FrontArticulo();
-<<<<<<< HEAD
+//		FrontAlquilar  alquilar= new FrontAlquilar();
+//		FrontActualizarBorrar AB = new FrontActualizarBorrar();
 		FrontLogin login = new FrontLogin(); 
 //		FrontArticulo articulo = new FrontArticulo();
 //		FrontAdmin admin = new FrontAdmin();
-=======
-	//FrontAdmin admin = new FrontAdmin();
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
 //		FrontPrestamo prestamo = new FrontPrestamo();
 //		FrontActualizarBorrarCD actualizarCD = new FrontActualizarBorrarCD();
 //		FrontActualizarBorrarDVD actualizarDVD = new FrontActualizarBorrarDVD();
