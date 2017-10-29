ef3750bf85d34f6623514880affb9b3c5e59e3b7
ODINCITO <ODINCITO@DESKTOP-BIU52JO>
2017-10-29 21:09:21 +0100

    FrontActores ok

--- src/MainCentroCultural.java
+++ src/MainCentroCultural.java
@@ -8,35 +8,28 @@ public class MainCentroCultural {
 //		FrontLogin login = new FrontLogin(); 
 //		FrontAlquilar  alquilar= new FrontAlquilar();
 //		FrontActualizarBorrar AB = new FrontActualizarBorrar();
-<<<<<<< HEAD
-		FrontLogin login = new FrontLogin(); 
-=======
+
+//		FrontLogin login = new FrontLogin(); 
+
 
 //	FrontLogin login = new FrontLogin();
 	FrontActores act= new FrontActores(null);
 
 //		FrontLogin login = new FrontLogin(); 
 
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
+
 //		FrontAdmin admin = new FrontAdmin();
 //		FrontPrestamo prestamo = new FrontPrestamo();
 //		FrontActualizarBorrarCD actualizarCD = new FrontActualizarBorrarCD();
 //		FrontActualizarBorrarDVD actualizarDVD = new FrontActualizarBorrarDVD();
 //		FrontActualizarBorrarLibro actualizarLibro = new FrontActualizarBorrarLibro();
 //		FrontGestionar  gs = new FrontGestionar();
-//		FrontCrearArticulo cr= new FrontCrearArticulo();
-<<<<<<< HEAD
+//		FrontCrearArticulo cr= new FrontCrearArticulo(null);
 		//FrontUsuario ffu= new FrontUsuario();
-//		FrontActores act= new FrontActores();
-=======
 //FrontUsuario ffu= new FrontUsuario();
 	
 
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
 //		FrontUsuario ffu= new FrontUsuario();
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/Pianek/CentroCultural1.git
 	}
 }
